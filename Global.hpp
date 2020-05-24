1b49e2a94bd13a7b28fc403492188fda7988f8f0
danny-326 <60639743+danny-326@users.noreply.github.com>
2020-05-24 20:15:19 +0800

    Add files via upload

---
+++ Global.hpp
@@ -0,0 +1,13 @@
+//
+//  Global.hpp
+//  QingNianDaXueXi
+//
+//  Created by Danny on 2020/5/23.
+//  Copyright © 2020 Danny. All rights reserved.
+//
+
+#ifndef Global_h
+#define Global_h
+#define GETURL_MATCH_PATTERN
+
+#endif /* Global_h */
